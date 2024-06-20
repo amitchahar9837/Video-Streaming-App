773b5c950ad71bb87ada5dc348443840b5052c69
amitchahar9837 <amitchahar9837@gmail.com>
2024-06-20 23:37:25 +0530

    Some files are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div style={{width:"90vw", height:'90vh'}}>
+      <video style={{width:"100%", height:"100%"}} src="http://localhost:3000/video" controls></video>
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,44 @@
 import express from "express";
+import { createReadStream, statSync } from "fs";
 import {dirname} from 'path'
 import { fileURLToPath } from "url";
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+console.log(__dirname)
 app.get('/',(req,res)=>{
       res.send("Server running")
 })
 
 app.get('/video',(req,res)=>{
-      const file = `${__dirname}/video.mp4`;
-      res.send(file);
+      const file = `${__dirname}/public/video.mkv`;
+      const range = req.headers.range;
+      const stat = statSync(file);
+      const fileSize = stat.size;
+      if(!range){
+           return res.status(400).send("Requires Range header")
+      }
+
+      const chunkSize = 10**6;
+      const start = Number(range.replace(/\D/g,""));
+      const end = Math.min(start + chunkSize, fileSize - 1);
+      const contentLength = end - start + 1
+
+      const header = {
+            "Content-Range" : `bytes ${start}-${end}/${fileSize}`,
+            "Accept-Ranges" : "bytes",
+            "Content-Length" : contentLength,
+            "Content-Type" : "video/mp4"
+      }
+      
+      res.writeHead(206, header)
+      const fileStream = createReadStream(file,{
+            start,
+            end
+      })
+      fileStream.pipe(res)
 })
 
 app.listen(3000,()=>{
